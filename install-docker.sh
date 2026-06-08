27ad5804cbe9043896da496b815fb01de054c347
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2026-06-08 16:51:47 +0200

    install-docker

--- install-docker.sh
+++ install-docker.sh
@@ -2,6 +2,7 @@
 
 R="\e[31m"
 N="\e[0m"
+
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
